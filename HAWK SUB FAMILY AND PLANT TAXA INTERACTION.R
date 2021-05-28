18a01e97ffe21f3a6674b4cd8c5efe780b4c2bec
DIPU12514 <78730798+DIPU12514@users.noreply.github.com>
2021-05-28 12:00:51 +0530

    Add files via upload

---
+++ HAWK SUB FAMILY AND PLANT TAXA INTERACTION.R
@@ -0,0 +1,6 @@
+install.packages("circlize")
+library(circlize)
+pollen <- read.csv("CHORD DIAGRAM HAWK MOTH.csv")
+pollen
+grid.col = c(Sphinginae = "green", Macroglossinae = "red",Smerinthinae = "blue", Poa = "grey", Brass  = "black", Ros = "sky blue", Acan = "navy blue", Ara = "violet", Sal = "orange", Ast="red",Bals="green",Betu="yellow",Verb="brown",Mal="wheat",Eup="sea green",Ole="Maroon",Eri="red",Elae="green", Fab="black")
+chordDiagram(pollen,grid.col = grid.col)
