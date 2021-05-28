95fe6d4963938a836c3ae88fa708e871e1fcc462
DIPU12514 <78730798+DIPU12514@users.noreply.github.com>
2021-05-28 11:57:59 +0530

    Delete final chord diagram.R

--- final chord diagram.R
+++
@@ -1,6 +0,0 @@
-install.packages("circlize")
-library(circlize)
-pollen <- read.csv("CHORD DIAGRAM HAWK MOTH.csv")
-pollen
-grid.col = c(Sphingidae = "green", Macroglossinae = "red",Smerinthinae = "blue", Poa = "grey", Brass  = "black", Ros = "sky blue", Acan = "navy blue", Ara = "violet", Sal = "orange", Ast="red",Bals="green",Betu="yellow",Verb="brown",Mal="wheat",Eup="sea green",Ole="Maroon",Eri="red",Elae="green", Fab="black")
-chordDiagram(pollen,grid.col = grid.col)
