2bb0e37ec86a543dc988f3823becf4168376e448
DIPU12514 <78730798+DIPU12514@users.noreply.github.com>
2021-08-03 13:45:26 +0530

    Update new pollen spectram.R

--- new pollen spectram.R
+++ new pollen spectram.R
@@ -1,6 +1,6 @@
 install.packages("rioja")
 library("rioja")
-POLLEN <- read.csv("Book1.csv",header=TRUE)
+POLLEN <- read.csv("new spectram.csv",header=TRUE)
 POLLEN1 <-POLLEN[-1]
 y.scale <- 1:65
 p.col.group <- c(rep("blue", times=11), rep("red", times=9), rep("green", times=10), rep("brown", times=7),rep("violet",times=4), rep("yellow",times=5),rep("black",times=9), rep("orange",times=2),rep("grey",times=8))
